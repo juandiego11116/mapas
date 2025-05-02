ca9e811105a7774a52524c0fb05cec4f086a497f
juandiego11116@gmail.com <71513912+juandiego11116@users.noreply.github.com>
2025-05-02 14:54:09 -0500

    Add initial project configuration files: Manifest, Gradle setup, and versioning

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,6 +16,14 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        gradlePluginPortal()
+        maven {
+            url = uri("https://api.mapbox.com/downloads/v2/releases/maven")
+            credentials {
+                username = "mapbox"
+                password = "pk.eyJ1IjoianVhbmRpZWdvMTExMTYiLCJhIjoiY21hMXAwZTRuMWphbzJqb2wyNmx6NXp1NiJ9.E-xIaG7Jz635dJcQVxzpDg"
+            }
+        }
     }
 }
 
